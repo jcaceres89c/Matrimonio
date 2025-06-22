cc19d645cfe7e2d5e80bf2bd72551a9cf1c3254e
Jorge Caceres <ori_jcaceres@niubiz.com.pe>
2025-06-21 23:18:57 -0500

    Correxion Audio android v2

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,17 @@
-const myAudio = document.getElementById("background-audio");
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("background-audio");
 
-function iniciarAudio() {
-  myAudio.play().catch(e => console.log("Error al intentar reproducir el audio:", e));
-  document.removeEventListener("click", iniciarAudio);
-  document.removeEventListener("touchstart", iniciarAudio);
-}
+  function iniciarAudio() {
+    if (audio.paused) {
+      audio.play().catch((e) => {
+        console.log("Error al reproducir audio:", e);
+      });
+    }
+    // Eliminar listeners una vez activado
+    document.removeEventListener("click", iniciarAudio);
+    document.removeEventListener("touchstart", iniciarAudio);
+  }
 
-document.addEventListener("click", iniciarAudio);
-document.addEventListener("touchstart", iniciarAudio);
+  document.addEventListener("click", iniciarAudio);
+  document.addEventListener("touchstart", iniciarAudio);
+});
